34e82d17041339488fd550cae1813ab5f82a574f
Chetan <chetan.bhosale@nonstopio.com>
2026-02-12 12:59:25 +0530

    Initial commit.

---
+++ firstspring-app/src/main/java/com/firstspring_app/firstspring_app/FirstspringAppApplication.java
@@ -0,0 +1,13 @@
+package com.firstspring_app.firstspring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstspringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstspringAppApplication.class, args);
+	}
+
+}
---
+++ firstspring-app/src/test/java/com/firstspring_app/firstspring_app/FirstspringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstspring_app.firstspring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstspringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
